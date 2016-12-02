9f50ce0e58ef8d19b5005c495be77247c968947e
Cameron Whiting <thetoxicarcade@gmail.com>
2016-12-02 04:00:07 -0700

    little bit of reorganizing

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Congredi
-> a law-via-cryptography application
+> a BASE representation-of-law-via-cryptography protocol
 
 Congredi is a Twisted protocol for groups to representationally agree on things.
 
-This application uses DHT/.onion rendesvous to provide peerage, and some DB.
-
-This is the functionality for how they'd talk together, not how a client
-would go about using it, or a provider scaling it. I leave that to you.
+* Requirements: Python Cryptography (Fernet & EC), Twisted
+* Addons: Stem, Flask, PyJWT
 
 Hopefully, with any luck, the files written here will be well-documented, unit
 tested, & cohesive/decoupled.
 
+I leave design of a perusing/issuing client to you.
 
--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup
 def readme():
 	with open('README.md') as f:
 		return f.read()
-setup(name='delegito',
+setup(name='congredi',
 	version='0.1',
-	description='Single Transferable Voting service',
-	long_description='Delegito is the python module for an STV API called congredi.\
-	This is currently a work in progress, so the Shuffle-Sum & Secure Secret Sharing\
-	are not implemented as directly as need be. I\'m also terrible at interfaces...', #readme()
+	description='BASE representation-of-law-via-cryptography protocol',
+	long_description=readme()
 	classifiers=[
 		'Development Status :: 1 - Planning',
 		'Intended Audience :: Science/Research',
@@ -18,12 +16,12 @@ setup(name='delegito',
 		'Operating System :: POSIX :: Linux',
 		'Topic :: Security :: Cryptography'
 		],
-	keywords='STV Single Transferable Voting',
+	keywords='STV Single Transferable Voting Crypto Twisted',
 	url='https://github.com/thetoxicarcade/congredi',
 	author='Cameron Whiting',
 	author_email='thetoxicarcade@gmail.com',
 	license='GPL3',
-	packages=['delegito'],
+	packages=['congredi'],
 	install_requires=[
 		'flask',
 		'pymongo',
@@ -32,8 +30,6 @@ setup(name='delegito',
 		'pyjwt'
 		],
 	include_package_data=True,
-	#https://tom-christie.github.io/articles/pypi/
-	#http://stackoverflow.com/questions/15650331/how-to-include-package-sub-folders-in-my-project-distribution/28298339
 	#packages = ['.','delegito','docs','security','service','tests'],
 	#package_data={'delegito':['*'],'docs':['*/*'],},
 	test_suite='nose2.collector.collector',
