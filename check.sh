e63d782969a6ed4445f603d666a6799b4441b846
thetoxicarcade <thetoxicarcade@gmail.com>
2016-12-28 00:10:24 -0700

    test

--- check.sh
+++ check.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+git stash -q --keep-index
+set +e
+#trap 'git stash pop -q' EXIT
 while read line; do
 	find . -name "$line" -prune -print -exec rm -rv {} \;
 done < .gitignore;
@@ -11,3 +14,19 @@ nose2 congredi
 #mkdocs gh-deploy || echo "Docs failing..."
 python setup.py build bdist_wheel 1>/dev/null
 find congredi/ -type f -name "*.pyc" -delete -print
+RESULT=$?
+git stash pop -q
+[ $RESULT -ne 0 ] && echo "Commit aborted!" exit 1
+exit 0
+
+# #!/bin/bash
+
+# set +e
+# bash -e <<TRY
+#   echo hello
+#   cd /does/not/exist
+#   echo world
+# TRY
+# if [ $? -ne 0 ]; then
+#   echo caught exception
+# fi
