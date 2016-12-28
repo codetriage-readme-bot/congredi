21eca92f4ba47bb4009d3f78212844bba24eab27
thetoxicarcade <thetoxicarcade@gmail.com>
2016-12-28 00:20:48 -0700

    githooks / styling stuff

--- check.sh
+++ check.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
+# Install this check into your git hooks...
+# ln -s -f ../../check.sh .git/hooks/pre-commit
 git stash --keep-index
 set +e
 #trap 'git stash pop -q' EXIT
 while read line; do
-	find . -name "$line" -prune -print -exec rm -rv {} \;
+	find . -name "$line" -prune -print -exec rm -r {} \;
 done < .gitignore;
 chmod -x congredi/ -R 
 pip install -q autopep8 nose2 setuptools-lint pylint
-autopep8 -rvi congredi
-pylint congredi || echo "Lint failed..."
+autopep8 -rvi congredi 1>/dev/null
+pylint congredi 1>/dev/null || echo "Lint failed..."
 pip install -q -r requirements.txt
-nose2 congredi
+nose2 congredi 1>/dev/null
 #mkdocs gh-deploy || echo "Docs failing..."
 python setup.py build bdist_wheel 1>/dev/null
-find congredi/ -type f -name "*.pyc" -delete -print
+find congredi/ -type f -name "*.pyc" -delete
 RESULT=$?
 git stash pop -q
 [ $RESULT -ne 0 ] && echo "Commit aborted!" exit 1
@@ -23,9 +25,7 @@ exit 0
 
 # set +e
 # bash -e <<TRY
-#   echo hello
 #   cd /does/not/exist
-#   echo world
 # TRY
 # if [ $? -ne 0 ]; then
 #   echo caught exception
--- docs/contributing/style.md
+++ docs/contributing/style.md
@@ -6,7 +6,8 @@ from `eslint` or `pylint`, but I could be.
 
 ## Python
 
-Pep8 excepting Tabs. Running `check.sh` can replace lots of it.
+Pep8 via autopep8. Note that I come from a lovely tabs world, so
+I just suggest installing `check.sh` into your git hooks.
 
 ## JS
 
